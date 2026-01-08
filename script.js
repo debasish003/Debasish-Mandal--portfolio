03e1f35dcee4f03de2e6a5a96c1ea114711e213d
Debasish <debasish41819@gamil.com>
2026-01-08 20:13:30 +0530

    update

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const menuIconMenu = document.getElementById('iconForNav');
 const navIconSwitch = document.getElementById('nav-icon');
 const modeBtn = document.getElementById('mode');
 const bodyClass = document.getElementById('main-body');
+bodyClass.classList.add('mainDark-background-img');
 console.log(bodyClass);
 console.log(modeBtn.className);
 console.log(menuIconMenu.className);
@@ -27,22 +28,20 @@ console.log(menuIconMenu.className);
 
 console.log(menuIconMenu.className);
 //<i class="fa-solid fa-xmark"></i> cross icon
-modeBtn.addEventListener('click',()=>{
+modeBtn.addEventListener('click',(e)=>{
     if(modeBtn.className =='bi bi-brightness-low-fill'){
         modeBtn.classList.add('bi-moon-fill');
         modeBtn.classList.remove('bi-brightness-low-fill');
-        // bodyClass.style.backgroundImage = 'none';
-        // bodyClass.style.backgroundImage = "url('whiteBackground.jpg')";
-        // bodyClass.style.bodySize = 'cover';
-        // bodyClass.style.backgroundAttachment = 'fixed';
+       bodyClass.classList.add('mainLight-background-img');
+        bodyClass.classList.remove('mainDrark-background-img');
         
     }
     else{
         modeBtn.classList.add('bi-brightness-low-fill');
         modeBtn.classList.remove('bi-moon-fill');
-        // bodyClass.style.backgroundImage = 'none';
-        // bodyClass.style.backgroundImage = "url('./logos/blackBackground.jpg')";
-        // bodyClass.style.backgroundAttachment = 'fixed';
+         bodyClass.classList.add('mainDrark-background-img');
+        bodyClass.classList.remove('mainLight-background-img');
     }
 });
+
 /* <i class="bi bi-moon-fill"></i>   moon icon*/
